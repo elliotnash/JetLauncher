6260390d963d5d22a7078cf2c4341ed54ce587e2
Elliot Nash <elliotnash@gmail.com>
2022-02-02 16:29:31 -0800

    and we're launchin

--- app/src/main/java/org/elliotnash/jetlauncher/LauncherActivity.kt
+++ app/src/main/java/org/elliotnash/jetlauncher/LauncherActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,11 +28,8 @@ class LauncherActivity : ComponentActivity() {
         setContent {
             JetLauncherTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text("I'm dumb")
+                Box(Modifier.fillMaxSize()) {
+                    Text("Centered", modifier = Modifier.align(Alignment.Center))
                 }
             }
         }
